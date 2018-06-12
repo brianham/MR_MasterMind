a6add8838b26f68166eb7d7207d1ef34aa34b5c6
Brian Hampson <nospmah@hotmail.com>
2018-06-12 16:44:10 -0700

    Adding LUIS scripting

--- Assets/Scripts/Gaze.cs
+++ Assets/Scripts/Gaze.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Gaze : MonoBehaviour {
+public class Gaze : MonoBehaviour
+{
+    internal GameObject gazedObject;
+    public float gazeMaxDistance = 300;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
+        // Uses a raycast from the Main Camera to determine which object is gazed upon.
+        Vector3 fwd = gameObject.transform.TransformDirection(Vector3.forward);
+        Ray ray = new Ray(Camera.main.transform.position, fwd);
+        RaycastHit hit;
+        Debug.DrawRay(Camera.main.transform.position, fwd);
+
+        if (Physics.Raycast(ray, out hit, gazeMaxDistance) && hit.collider != null)
+        {
+            if (gazedObject == null)
+            {
+                gazedObject = hit.transform.gameObject;
+
+                // Set the gazedTarget in the Behaviours class
+                Behaviours.instance.gazedTarget = gazedObject;
+            }
+        }
+        else
+        {
+            ResetGaze();
+        }
+    }
+
+    // Turn the gaze off, reset the gazeObject in the Behaviours class.
+    public void ResetGaze()
+    {
+        if (gazedObject != null)
+        {
+            Behaviours.instance.gazedTarget = null;
+            gazedObject = null;
+        }
+    }
 }
